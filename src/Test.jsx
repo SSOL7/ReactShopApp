6960a30aa1a2dad4ea2aeba7346665b0eafea94a
SSOL7 <soltani.shaho@gmail.com>
2023-08-18 12:08:50 +0300

    initialized repo for shopping app

---
+++ src/Test.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Button from 'react-bootstrap/Button';
+
+function Test() {
+  return (
+    <div>
+        <Button variant="secondary">Primary</Button>
+    </div>
+  )
+}
+
+export default Test
